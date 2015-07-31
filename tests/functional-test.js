b8986e094e3668ba430f3aed0caf9de2589df16a
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2015-07-31 23:11:47 +0530

    more functional tests

--- tests/functional-test.js
+++ tests/functional-test.js
@@ -15,6 +15,7 @@ import BroccoliLeasotFilter from '../src/index';
 let expect = chai.expect;
 chai.use(sinonChai);
 
+
 describe('Functional test suite for Broccoli Leasot', () => {
 	let broccoliLeasot;
 	let fileTree;
@@ -24,9 +25,17 @@ describe('Functional test suite for Broccoli Leasot', () => {
 			message += stripAnsi(msg);
 		}
 	};
+	
+	let checkWithMarkerForGroupByFile = (marker) => {
+		expect(message).to.include(marker.trim());
+	};
 
-	beforeEach(() => {
-		fileTree = fixtureTree({
+	let checkWithMarkerForGroupByKind = (marker) => {
+		expect(message).to.include(marker.trim().replace('FIXME','').replace('TODO','').replace('CUSTOM_TAG',''));
+	};
+
+	let setupFixtureTree = function(includeImaginaryExtension = false) {
+		let tree = {
 			app: {
 				css: {
 					'application.css': fixtures.getCssContent()
@@ -38,15 +47,23 @@ describe('Functional test suite for Broccoli Leasot', () => {
 					'index.handlebars': fixtures.getHbsContent()
 				}
 			}
-		});
-	});
+		};
+		if (includeImaginaryExtension) {
+			tree.app['warriors'] = {
+				'rocky.balboa': ''
+			};
+		}
+		fileTree = fixtureTree(tree);
+	};
+
 
 	afterEach(() => {
 		message = '';
+		fileTree = null;
 	});
 
-	it('Should work', () => {
-
+	it('Basic setup', () => {
+		setupFixtureTree();
 		broccoliLeasot = new BroccoliLeasotFilter(fileTree, {
 			enabled: true,
 			kinds: ['TODO', 'FIXME', 'CUSTOM_TAG'],
@@ -54,13 +71,42 @@ describe('Functional test suite for Broccoli Leasot', () => {
 		});
 		let outputTree = new BroccoliBuilder(broccoliLeasot);
 		return outputTree.build().then(function() {
-			let checkWithMarker = (marker) => {
-				expect(message).to.include(marker.trim());
-			};
+			fixtures.jsMarkers.forEach(checkWithMarkerForGroupByFile);
+			fixtures.cssMarkers.forEach(checkWithMarkerForGroupByFile);
+			fixtures.hbsMarkers.forEach(checkWithMarkerForGroupByFile);
+		});
 
-			fixtures.jsMarkers.forEach(checkWithMarker);
-			fixtures.cssMarkers.forEach(checkWithMarker);
-			fixtures.hbsMarkers.forEach(checkWithMarker);
+	});
+
+
+	it('Group by type', () => {
+		setupFixtureTree();
+		broccoliLeasot = new BroccoliLeasotFilter(fileTree, {
+			enabled: true,
+			kinds: ['TODO', 'FIXME', 'CUSTOM_TAG'],
+			groupBy: 'kind',
+			console: konsole
+		});
+		let outputTree = new BroccoliBuilder(broccoliLeasot);
+		return outputTree.build().then(function() {
+			fixtures.jsMarkers.forEach(checkWithMarkerForGroupByKind);
+			fixtures.cssMarkers.forEach(checkWithMarkerForGroupByKind);
+			fixtures.hbsMarkers.forEach(checkWithMarkerForGroupByKind);
+		});
+
+	});
+
+	it('Imaginary extension lookup', () => {
+		setupFixtureTree(true);
+		broccoliLeasot = new BroccoliLeasotFilter(fileTree, {
+			enabled: true,
+			extensions: ['balboa'],
+			console: konsole
+		});
+		let outputTree = new BroccoliBuilder(broccoliLeasot);
+		return outputTree.build().then(function() {
+			let linkToLeasotDocs = 'https://github.com/pgilad/leasot#supported-languages';
+			expect(message).to.include(linkToLeasotDocs);
 		});
 
 	});
